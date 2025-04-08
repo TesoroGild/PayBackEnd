96c03586ceb3f2fdd00d05617924ef36730a55c6
Vito <gilboss85@gmail.com>
2025-04-08 11:24:53 -0400

    v1

--- src/main/java/calcul/Refund.java
+++ src/main/java/calcul/Refund.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Refund {
     public static List<Dollar> amountRefund(String contract, List<Reclamation> reclamations) throws ApplicationException {
+        System.out.println(contract);
         if (contract.equals("A")) return contractA(reclamations);
         if (contract.equals("B")) return contractB(reclamations);
         if (contract.equals("C")) return contractC(reclamations);
@@ -18,11 +19,13 @@ public class Refund {
     private static List<Dollar> contractA (List<Reclamation> reclamations) throws ApplicationException {
         List<Dollar> amountsRefA = new ArrayList<>();
         for (Reclamation reclamation : reclamations) {
+            System.out.println(300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399);
+            System.out.println(reclamation);
             if ("0".equals(reclamation.healthCare()) || "100".equals(reclamation.healthCare())
                     || "200".equals(reclamation.healthCare()) || "500".equals(reclamation.healthCare())) amountsRefA.add(treatmentA25Percent(reclamation.amount()));
-            if ((300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399)
+            else if ((300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399)
                     || "400".equals(reclamation.healthCare()) || "700".equals(reclamation.healthCare())) amountsRefA.add(new Dollar("00.00$"));
-            if ("600".equals(reclamation.healthCare())) amountsRefA.add(treatmentA40Percent(reclamation.amount()));
+            else if ("600".equals(reclamation.healthCare())) amountsRefA.add(treatmentA40Percent(reclamation.amount()));
             else throw new ApplicationException("Catégorie de soin inattendue.");
         }
         return amountsRefA;
@@ -32,12 +35,12 @@ public class Refund {
         List<Dollar> amountsRefB = new ArrayList<>();
         for (Reclamation reclamation : reclamations) {
             if ("0".equals(reclamation.healthCare())) amountsRefB.add(treatmentB50Percent40Max(reclamation.amount()));
-            if ("100".equals(reclamation.healthCare()) || "500".equals(reclamation.healthCare())) amountsRefB.add(treatmentB50Percent50Max(reclamation.amount()));
-            if ("200".equals(reclamation.healthCare())) amountsRefB.add(treatmentB100Percent70Max(reclamation.amount()));
-            if ((300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399)) amountsRefB.add(treatmentB50PercentNoMax(reclamation.amount()));
-            if ("400".equals(reclamation.healthCare())) amountsRefB.add(new Dollar("00.00$"));
-            if ("600".equals(reclamation.healthCare())) amountsRefB.add(treatmentB100PercentNoMax(reclamation.amount()));
-            if ("700".equals(reclamation.healthCare())) amountsRefB.add(treatmentB70PercentNoMax(reclamation.amount()));
+            else if ("100".equals(reclamation.healthCare()) || "500".equals(reclamation.healthCare())) amountsRefB.add(treatmentB50Percent50Max(reclamation.amount()));
+            else if ("200".equals(reclamation.healthCare())) amountsRefB.add(treatmentB100Percent70Max(reclamation.amount()));
+            else if ((300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399)) amountsRefB.add(treatmentB50PercentNoMax(reclamation.amount()));
+            else if ("400".equals(reclamation.healthCare())) amountsRefB.add(new Dollar("00.00$"));
+            else if ("600".equals(reclamation.healthCare())) amountsRefB.add(treatmentB100PercentNoMax(reclamation.amount()));
+            else if ("700".equals(reclamation.healthCare())) amountsRefB.add(treatmentB70PercentNoMax(reclamation.amount()));
             else throw new ApplicationException("Catégorie de soin inattendue.");
         }
         return amountsRefB;
@@ -59,11 +62,11 @@ public class Refund {
         List<Dollar> amountsRefD = new ArrayList<>();
         for (Reclamation reclamation : reclamations) {
             if ("0".equals(reclamation.healthCare())) amountsRefD.add(treatmentD85Max(reclamation.amount()));
-            if ("100".equals(reclamation.healthCare()) || "500".equals(reclamation.healthCare())) amountsRefD.add(treatmentD75Max(reclamation.amount()));
-            if ("200".equals(reclamation.healthCare()) || "600".equals(reclamation.healthCare())) amountsRefD.add(treatmentD100Max(reclamation.amount()));
-            if (300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399) amountsRefD.add(new Dollar(reclamation.amount()));
-            if ("400".equals(reclamation.healthCare())) amountsRefD.add(treatmentD65Max(reclamation.amount()));
-            if ("700".equals(reclamation.healthCare())) amountsRefD.add(treatmentD90Max(reclamation.amount()));
+            else if ("100".equals(reclamation.healthCare()) || "500".equals(reclamation.healthCare())) amountsRefD.add(treatmentD75Max(reclamation.amount()));
+            else if ("200".equals(reclamation.healthCare()) || "600".equals(reclamation.healthCare())) amountsRefD.add(treatmentD100Max(reclamation.amount()));
+            else if (300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399) amountsRefD.add(new Dollar(reclamation.amount()));
+            else if ("400".equals(reclamation.healthCare())) amountsRefD.add(treatmentD65Max(reclamation.amount()));
+            else if ("700".equals(reclamation.healthCare())) amountsRefD.add(treatmentD90Max(reclamation.amount()));
             else throw new ApplicationException("Catégorie de soin inattendue.");
         }
         return amountsRefD;
