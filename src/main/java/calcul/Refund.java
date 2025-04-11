4e0f37a36a72522106269b548d871b58d1142510
Vito <gilboss85@gmail.com>
2025-04-11 14:58:20 -0400

    refactor refund methods

--- src/main/java/calcul/Refund.java
+++ src/main/java/calcul/Refund.java
@@ -24,27 +24,17 @@ public class Refund {
         for (Reclamation reclamation : reclamations) {
             if ("0".equals(reclamation.healthCare())) amountsRefundA.add(getReclamationAmount(reclamation.amount(), 25));
             else if ("100".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundA, reclamation.healthCare(), reclamation.amount(), 25000, 35);
-                totalRefundA.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundA.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundA, amountsRefundA, reclamation.healthCare(), reclamation.amount(), 25000, 35);
             } else if ("150".equals(reclamation.healthCare()) ||(300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399)
                     || "400".equals(reclamation.healthCare()) || "700".equals(reclamation.healthCare())) amountsRefundA.add(new Dollar("00.00$"));
             else if ("175".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundA, reclamation.healthCare(), reclamation.amount(), 20000, 50);
-                totalRefundA.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundA.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundA, amountsRefundA, reclamation.healthCare(), reclamation.amount(), 20000, 50);
             } else if ("200".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundA, reclamation.healthCare(), reclamation.amount(), 25000, 35);
-                totalRefundA.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundA.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundA, amountsRefundA, reclamation.healthCare(), reclamation.amount(), 25000, 35);
             } else if ("500".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundA, reclamation.healthCare(), reclamation.amount(), 15000, 25);
-                totalRefundA.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundA.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundA, amountsRefundA, reclamation.healthCare(), reclamation.amount(), 15000, 25);
             } else if ("600".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundA, reclamation.healthCare(), reclamation.amount(), 30000, 40);
-                totalRefundA.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundA.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundA, amountsRefundA, reclamation.healthCare(), reclamation.amount(), 30000, 40);
             } else throw new ApplicationException("Numéro de soin inattendue.");
         }
         return amountsRefundA;
@@ -56,29 +46,18 @@ public class Refund {
         for (Reclamation reclamation : reclamations) {
             if ("0".equals(reclamation.healthCare())) amountsRefundB.add(getReclamationAmountMax(reclamation.amount(), 50, 4000));
             else if ("100".equals(reclamation.healthCare())) {
-                Object[] val = getNewMaxAmountAndTotal(totalRefundB, reclamation.healthCare(), reclamation.amount(), 25000,  5000,50);
-                totalRefundB.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundB.add((Dollar) val[0]);
+                getMaxRefundAndTotal(totalRefundB, amountsRefundB, reclamation.healthCare(), reclamation.amount(), 25000,  5000,50);
             } else if ("150".equals(reclamation.healthCare()) || "400".equals(reclamation.healthCare())) amountsRefundB.add(new Dollar("00.00$"));
             else if ("175".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundB, reclamation.healthCare(), reclamation.amount(), 20000,75);
-                totalRefundB.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundB.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundB, amountsRefundB, reclamation.healthCare(), reclamation.amount(), 20000,75);
             } else if ("200".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundB, reclamation.healthCare(), reclamation.amount(), 25000,  100);
-                totalRefundB.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundB.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundB, amountsRefundB, reclamation.healthCare(), reclamation.amount(), 25000,  100);
             } else if ((300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399)) amountsRefundB.add(getReclamationAmount(reclamation.amount(), 50));
             else if ("500".equals(reclamation.healthCare())) {
-                Object[] val = getNewMaxAmountAndTotal(totalRefundB, reclamation.healthCare(), reclamation.amount(), 15000,  5000,50);
-                totalRefundB.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundB.add((Dollar) val[0]);
+                getMaxRefundAndTotal(totalRefundB, amountsRefundB, reclamation.healthCare(), reclamation.amount(), 15000,  5000,50);
             } else if ("600".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundB, reclamation.healthCare(), reclamation.amount(), 30000,  100);
-                totalRefundB.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundB.add((Dollar) val[0]);
-            }
-            else if ("700".equals(reclamation.healthCare())) amountsRefundB.add(getReclamationAmount(reclamation.amount(), 70));
+                getRefundAndTotal(totalRefundB, amountsRefundB, reclamation.healthCare(), reclamation.amount(), 30000,  100);
+            } else if ("700".equals(reclamation.healthCare())) amountsRefundB.add(getReclamationAmount(reclamation.amount(), 70));
             else throw new ApplicationException("Numéro de soin inattendue.");
         }
         return amountsRefundB;
@@ -93,26 +72,16 @@ public class Refund {
                     || (300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399))
                 amountsRefundC.add(getReclamationAmount(reclamation.amount(), 90));
             else if ("100".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundC, reclamation.healthCare(), reclamation.amount(), 25000, 95);
-                totalRefundC.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundC.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundC, amountsRefundC, reclamation.healthCare(), reclamation.amount(), 25000, 95);
             } else if ("150".equals(reclamation.healthCare())) amountsRefundC.add(getReclamationAmount(reclamation.amount(), 85));
             else if ("175".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundC, reclamation.healthCare(), reclamation.amount(), 20000, 90);
-                totalRefundC.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundC.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundC, amountsRefundC, reclamation.healthCare(), reclamation.amount(), 20000, 90);
             } else if ("200".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundC, reclamation.healthCare(), reclamation.amount(), 25000, 90);
-                totalRefundC.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundC.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundC, amountsRefundC, reclamation.healthCare(), reclamation.amount(), 25000, 90);
             } else if ("500".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundC, reclamation.healthCare(), reclamation.amount(), 15000, 90);
-                totalRefundC.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundC.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundC, amountsRefundC, reclamation.healthCare(), reclamation.amount(), 15000, 90);
             } else if ("600".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundC, reclamation.healthCare(), reclamation.amount(), 30000, 75);
-                totalRefundC.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundC.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundC, amountsRefundC, reclamation.healthCare(), reclamation.amount(), 30000, 75);
             } else throw new ApplicationException("Numéro de soin inattendue.");
         }
         return amountsRefundC;
@@ -124,28 +93,18 @@ public class Refund {
         for (Reclamation reclamation : reclamations) {
             if ("0".equals(reclamation.healthCare())) amountsRefundD.add(getReclamationAmountMax(reclamation.amount(), 100, 8500));
             else if ("100".equals(reclamation.healthCare())) {
-                Object[] val = getNewMaxAmountAndTotal(totalRefundD, reclamation.healthCare(), reclamation.amount(), 25000, 7500, 100);
-                totalRefundD.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundD.add((Dollar) val[0]);
+                getMaxRefundAndTotal(totalRefundD, amountsRefundD, reclamation.healthCare(), reclamation.amount(), 25000, 7500, 100);
             } else if ("150".equals(reclamation.healthCare())) amountsRefundD.add(getReclamationAmountMax(reclamation.amount(), 100, 15000));
             else if ("175".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundD, reclamation.healthCare(), reclamation.amount(), 20000, 95);
-                totalRefundD.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundD.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundD, amountsRefundD, reclamation.healthCare(), reclamation.amount(), 20000, 95);
             } else if ("200".equals(reclamation.healthCare())) {
-                Object[] val = getNewMaxAmountAndTotal(totalRefundD, reclamation.healthCare(), reclamation.amount(), 25000, 10000, 100);
-                totalRefundD.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundD.add((Dollar) val[0]);
+                getMaxRefundAndTotal(totalRefundD, amountsRefundD, reclamation.healthCare(), reclamation.amount(), 25000, 10000, 100);
             } else if (300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399) amountsRefundD.add(new Dollar(reclamation.amount()));
             else if ("400".equals(reclamation.healthCare())) amountsRefundD.add(getReclamationAmountMax(reclamation.amount(), 100, 6500));
             else if ("500".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundD, reclamation.healthCare(), reclamation.amount(), 15000, 100);
-                totalRefundD.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundD.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundD, amountsRefundD, reclamation.healthCare(), reclamation.amount(), 15000, 100);
             } else if ("600".equals(reclamation.healthCare())) {
-                Object[] val = getNewMaxAmountAndTotal(totalRefundD, reclamation.healthCare(), reclamation.amount(), 30000, 10000, 100);
-                totalRefundD.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundD.add((Dollar) val[0]);
+                getMaxRefundAndTotal(totalRefundD, amountsRefundD, reclamation.healthCare(), reclamation.amount(), 30000, 10000, 100);
             } else if ("700".equals(reclamation.healthCare())) amountsRefundD.add(getReclamationAmountMax(reclamation.amount(), 100, 9000));
             else throw new ApplicationException("Numéro de soin inattendue.");
         }
@@ -158,27 +117,17 @@ public class Refund {
         for (Reclamation reclamation : reclamations) {
             if ("0".equals(reclamation.healthCare()) || "150".equals(reclamation.healthCare())) amountsRefundE.add(getReclamationAmount(reclamation.amount(), 15));
             else if ("100".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundE, reclamation.healthCare(), reclamation.amount(), 25000, 25);
-                totalRefundE.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundE.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundE, amountsRefundE, reclamation.healthCare(), reclamation.amount(), 25000, 25);
             } else if ("175".equals(reclamation.healthCare())) {
-                Object[] val = getNewMaxAmountAndTotal(totalRefundE, reclamation.healthCare(), reclamation.amount(), 20000, 2000, 25);
-                totalRefundE.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundE.add((Dollar) val[0]);
+                getMaxRefundAndTotal(totalRefundE, amountsRefundE, reclamation.healthCare(), reclamation.amount(), 20000, 2000, 25);
             } else if ("200".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundE, reclamation.healthCare(), reclamation.amount(), 25000,120);
-                totalRefundE.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundE.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundE, amountsRefundE, reclamation.healthCare(), reclamation.amount(), 25000,120);
             } else if (300 <= Integer.parseInt(reclamation.healthCare()) && Integer.parseInt(reclamation.healthCare()) <= 399) amountsRefundE.add(getReclamationAmount(reclamation.amount(), 60));
             else if ("400".equals(reclamation.healthCare())) amountsRefundE.add(getReclamationAmountMax(reclamation.amount(), 25, 1500));
             else if ("500".equals(reclamation.healthCare())) {
-                Object[] val = getNewMaxAmountAndTotal(totalRefundE, reclamation.healthCare(), reclamation.amount(), 15000, 2000, 30);
-                totalRefundE.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundE.add((Dollar) val[0]);
+                getMaxRefundAndTotal(totalRefundE, amountsRefundE, reclamation.healthCare(), reclamation.amount(), 15000, 2000, 30);
             } else if ("600".equals(reclamation.healthCare())) {
-                Object[] val = getNewAmountAndTotal(totalRefundE, reclamation.healthCare(), reclamation.amount(), 30000, 15);
-                totalRefundE.put(reclamation.healthCare(), (Dollar) val[1]);
-                amountsRefundE.add((Dollar) val[0]);
+                getRefundAndTotal(totalRefundE, amountsRefundE, reclamation.healthCare(), reclamation.amount(), 30000, 15);
             } else if ("700".equals(reclamation.healthCare())) amountsRefundE.add(getReclamationAmount(reclamation.amount(), 22));
             else throw new ApplicationException("Numéro de soin inattendue.");
         }
@@ -197,7 +146,7 @@ public class Refund {
         return refund;
     }
 
-    private static Object[] getNewAmountAndTotal (Map<String, Dollar> totalRefund, String healthCare, String amount, long max, int percent) throws ApplicationException {
+    private static void getRefundAndTotal (Map<String, Dollar> totalRefund, List<Dollar> amountsRefund, String healthCare, String amount, long max, int percent) throws ApplicationException {
         long tmp;
         Dollar maxReclamation = totalRefund.getOrDefault(healthCare, new Dollar("00.00$"));
         Dollar prevAmount = new Dollar(amount);
@@ -207,10 +156,11 @@ public class Refund {
             newAmount.setCent(max - maxReclamation.getCent());
             maxReclamation.setCent(max);
         } else maxReclamation.setCent(tmp);
-        return new Object[] { newAmount, maxReclamation};
+        totalRefund.put(healthCare, maxReclamation);
+        amountsRefund.add(newAmount);
     }
 
-    private static Object[] getNewMaxAmountAndTotal (Map<String, Dollar> totalRefund, String healthCare, String amount, long maxTotal, long maxAmount, int percent) throws ApplicationException  {
+    private static void getMaxRefundAndTotal (Map<String, Dollar> totalRefund, List<Dollar> amountsRefund, String healthCare, String amount, long maxTotal, long maxAmount, int percent) throws ApplicationException  {
         long tmp;
         Dollar maxReclamation = totalRefund.getOrDefault(healthCare, new Dollar("00.00$"));
         Dollar prevAmount = new Dollar(amount);
@@ -221,6 +171,7 @@ public class Refund {
             newAmount.setCent(maxTotal - maxReclamation.getCent());
             maxReclamation.setCent(maxTotal);
         } else maxReclamation.setCent(tmp);
-        return new Object[] { newAmount, maxReclamation};
+        totalRefund.put(healthCare, maxReclamation);
+        amountsRefund.add(newAmount);
     }
 }
